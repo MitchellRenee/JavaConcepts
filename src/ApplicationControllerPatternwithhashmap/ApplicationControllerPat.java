bd95fb5d3ee08aa125e53e285132be1e2aa5b791
Renee' <A2icess@gmail.com>
2018-05-29 20:48:25 -0400

    Merge origin/master

--- src/ApplicationControllerPatternwithhashmap/ApplicationControllerPat.java
+++ src/ApplicationControllerPatternwithhashmap/ApplicationControllerPat.java
@@ -3,58 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ApplicationControllerPatternwithhashmap ;
+package ApplicationControllerPatternwithhashmap;
+
 /**
  *
  * @author Renee
-     * @param args
-     * @param i
+ * @param 
+ * @param 
  */
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Handler;
 public class ApplicationControllerPat {
-    private static final HashMap<String, Handler> handlerMap = new HashMap();
-    
-    public void handleCommand(String command, HashMap<String, Object> data){
-        Handler CommandHandler = handlerMap.get(command);
-        if (CommandHandler != null){
-            CommandHandler.getClass();
-        }
-    }
-    
-    public void mapCommand(String Command, Handler CommandHandler){
-        handlerMap.putIfAbsent(Command, CommandHandler);
-    }    
-    /*Declare a HashMap*/
-    HashMap<Integer,String> hmap = new HashMap<>();{
-    
-    /*Add elements for the HashMap*/
-    hmap.put(1,"Kirk Brown");
-    hmap.put(2,"Paul Darr");
-    hmap.put(3,"Edward Wilson");
-    hmap.put(4,"Mason Roller");
-    hmap.put(5,"Renee Mitchell");
 
-    /*Display output using an Iterator*/
-    Set set = hmap.entrySet();
-    Iterator iterator = set.iterator();
+
+    public void test() {
+
+        /*Declare a HashMap*/
+        HashMap<Integer, String> hmap = new HashMap<>();
+
+        /*Add elements for the HashMap*/
+        hmap.put(1, "Kirk Brown");
+        hmap.put(2, "Paul Darr");
+        hmap.put(3, "Edward Wilson");
+        hmap.put(4, "Mason Roller");
+        hmap.put(5, "Renee Mitchell");
+
+        /*Display output using an Iterator*/
+        Set set = hmap.entrySet();
+        Iterator iterator = set.iterator();
         int i = 0;
         Iterator<Map.Entry<Integer, String>> it = hmap.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry <Integer, String> pair = it.next();
-            /*a += pair.getKey() + pair.(); */       
-         System.out.println(pair.getKey() + pair.getValue());
-                 
-    
+        while (it.hasNext()) {
+            Map.Entry<Integer, String> pair = it.next();
+            /*a += pair.getKey() + pair.(); */
+            System.out.println(pair.getKey() + pair.getValue());
         }
-        
     }
-        /*Add function*/
-        public static void main(String args[]){    
-}
 
+    /*Add function*/
+    public static void main(String args[]) {
+        ApplicationControllerPat controller = new ApplicationControllerPat();
+        controller.test();
+
+    }
 }
